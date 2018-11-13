ca3a3fbdd406e37f3464d1d1066a105adf23a5fa
Nicholas G <nicholasgodoyx@gmail.com>
2018-11-12 22:40:41 -0200

    continuacao do inicio do inicio

--- T2/gera_codigo.c
+++ T2/gera_codigo.c
@@ -13,18 +13,14 @@ void gera_codigo(FILE *f, void **code, funcp *entry)
 {
 	int line = 1;
 	int  c;
-	FILE *myfp;
 
-	if ((myfp = fopen("programa", "r")) == NULL) {
-		perror("nao conseguiu abrir arquivo!");
-		exit(1);
-	}
-
-	while ((c = fgetc(myfp)) != EOF) {
+	int tamAtual = 0;
+	unsigned char *codea = malloc(tamAtual);
+	while ((c = fgetc(f)) != EOF) {
 		switch (c) {
 		case 'f': { /* function */
 			char c0;
-			if (fscanf(myfp, "unction%c", &c0) != 1) {
+			if (fscanf(f, "unction%c", &c0) != 1) {
 				error("comando invalido", line);
 			}
 			printf("function\n");
@@ -32,7 +28,7 @@ void gera_codigo(FILE *f, void **code, funcp *entry)
 		}
 		case 'e': { /* end */
 			char c0;
-			if (fscanf(myfp, "nd%c", &c0) != 1) {
+			if (fscanf(f, "nd%c", &c0) != 1) {
 				error("comando invalido", line);
 			}
 			printf("end\n");
@@ -41,7 +37,7 @@ void gera_codigo(FILE *f, void **code, funcp *entry)
 		case 'r': {  /* retorno incondicional */
 			int idx0, idx1;
 			char var0, var1;
-			if (fscanf(myfp, "et %c%d", &var0, &idx0) != 2) {
+			if (fscanf(f, "et %c%d", &var0, &idx0) != 2) {
 				error("comando invalido", line);
 			}
 			printf("ret %c%d\n", var0, idx0);
@@ -50,7 +46,7 @@ void gera_codigo(FILE *f, void **code, funcp *entry)
 		case 'z': {  /* retorno condicional */
 			int idx0, idx1;
 			char var0, var1;
-			if (fscanf(myfp, "ret %c%d %c%d", &var0, &idx0, &var1, &idx1) != 4) {
+			if (fscanf(f, "ret %c%d %c%d", &var0, &idx0, &var1, &idx1) != 4) {
 				error("comando invalido", line);
 			}
 			printf("zret %c%d %c%d\n", var0, idx0, var1, idx1);
@@ -59,21 +55,21 @@ void gera_codigo(FILE *f, void **code, funcp *entry)
 		case 'v': {  /* atribuicao */
 			int idx0;
 			char var0 = c, c0;
-			if (fscanf(myfp, "%d = %c", &idx0, &c0) != 2) {
+			if (fscanf(f, "%d = %c", &idx0, &c0) != 2) {
 				error("comando invalido", line);
 			}
 
 			if (c0 == 'c') { /* call */
 				int f, idx1;
 				char var1;
-				if (fscanf(myfp, "all %d %c%d\n", &f, &var1, &idx1) != 3) {
+				if (fscanf(f, "all %d %c%d\n", &f, &var1, &idx1) != 3) {
 					error("comando invalido", line);
 				}
 				printf("%c%d = call %d %c%d\n", var0, idx0, f, var1, idx1);
 			} else { /* operaÃ§Ã£o aritmÃ©tica */
 				int idx1, idx2;
 				char var1 = c0, var2, op;
-				if (fscanf(myfp, "%d %c %c%d", &idx1, &op, &var2, &idx2) != 4) {
+				if (fscanf(f, "%d %c %c%d", &idx1, &op, &var2, &idx2) != 4) {
 					error("comando invalido", line);
 				}
 				printf("%c%d = %c%d %c %c%d\n",
@@ -85,35 +81,32 @@ void gera_codigo(FILE *f, void **code, funcp *entry)
 			error("comando desconhecido", line);
 		}
 		line ++;
-		fscanf(myfp, " ");
+		fscanf(f, " ");
 	}
 	return;
 }
 
-void gera_codigo2(void **codigo, funcp *entry) {
-	unsigned char code[] = { 0x83, 0xc7, 0x01, 0x89, 0xf8, 0xc3 };
-	//unsigned char *code = malloc(6);
-	*entry = (funcp) code;/*
-	code[0] = 0x55;
-	code[1] = 0x48;
-	code[2] = 0x89;
-	code[3] = 0xe5;
-	code[4] = 0xb8;
-	code[5] = 0x64;
-	code[6] = 0x00;
-	code[7] = 0x00;
-	code[8] = 0x00;
-	code[9] = 0x48;
-	code[10] = 0x89;
-	code[11] = 0xec;
-	code[12] = 0x5d;
-	code[13] = 0xc3;*//*
-	code[0] = 0x83;
-	code[1] = 0xc7;
-	code[2] = 0x01;
-	code[3] = 0x89;
-	code[4] = 0xf8;
-	code[5] = 0xc3;*/
+void gera_codigo2(void **code, funcp *entry) {
+	unsigned char *codea = malloc(14);
+	*entry = (funcp) codea;
+	codea[0] = 0x55;
+	codea[1] = 0x48;
+	codea[2] = 0x89;
+	codea[3] = 0xe5;
+	codea[4] = 0xb8;
+	codea[5] = 0x64;
+	codea[6] = 0x00;
+	codea[7] = 0x00;
+	codea[8] = 0x00;
+	codea[9] = 0x48;
+	codea[10] = 0x89;
+	codea[11] = 0xec;
+	codea[12] = 0x5d;
+	codea[13] = 0xc3;
+
+	/* passa o array usado para o parametro recebido,
+	para ser liberado fora */
+	*code = codea;
 }
 
 void libera_codigo(void *p) {}
--- T2/teste.c
+++ T2/teste.c
@@ -1,30 +1,23 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef int(*funcp)(int x);
 
-unsigned char codigo[] = { 0x83, 0xc7, 0x01, 0x89, 0xf8, 0xc3 };
-
+void gera_codigo(FILE *f, void **code, funcp *entry);
 void gera_codigo2(void **code, funcp *entry);
 
 int main(void) {
 	void *cd;
-	funcp f = (funcp)codigo;
 	funcp entry;
-	printf("%d\n", (*f)(10));
+
 	gera_codigo2(&cd, &entry);
 	printf("%d\n", (*entry)(10));
+	free(cd);
 
-	return 0;
-}*/
-#include <stdio.h>
-unsigned char codigo[] = { 0x83, 0xc7, 0x01, 0x89, 0xf8, 0xc3 };
+	FILE *f = fopen("programa", "r");
+	if (f == NULL) exit(1);
 
-typedef int(*funcp) (int x);
+	gera_codigo(f, &cd, &entry);
 
-int main(void) {
-	funcp f = (funcp)codigo;
-
-	printf("%d\n", (*f)(10));
 	return 0;
 }
